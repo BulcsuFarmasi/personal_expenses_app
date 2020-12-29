186b173c78f341731880469cfcbafa8455f7c28c
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2020-12-29 16:36:06 +0100

    create dummy transactions

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,23 @@
 import 'package:flutter/material.dart';
+import './transaction.dart';
 
 void main() => runApp(App());
 
 class App extends StatelessWidget {
+  final List<Transaction> transaction = [
+    Transaction(
+      id: 't1',
+      title: 'New Shoes',
+      ammout: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Weekly Groceries',
+      ammout: 16.53,
+      date: DateTime.now(),
+    )
+  ];
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
