463d7c585b8ad3127be2ea01c68a54f327e840b6
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2021-01-01 22:22:53 +0100

    use single child scroll view

--- lib/main.dart
+++ lib/main.dart
@@ -10,23 +10,27 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(
-              title: Text('Personal Expenses'),
-            ),
-            body: Column(
-              // mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Container(
-                    width: double.infinity,
-                    child: Card(
-                      color: Colors.blue,
-                      child: Text('CHART!'),
-                      elevation: 5,
-                    )),
-                UserTransactions(),
-              ],
-            )));
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Personal Expenses'),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            // mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                  width: double.infinity,
+                  child: Card(
+                    color: Colors.blue,
+                    child: Text('CHART!'),
+                    elevation: 5,
+                  )),
+              UserTransactions(),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
