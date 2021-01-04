5aef77e2b13cd9293f89faccdebbb4b5ca7c70ee
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2021-01-04 19:55:46 +0100

    add method to sum recent transactions

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -1,11 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import '../models/transaction.dart';
 
 class Chart extends StatelessWidget {
+  Chart(this.recentTransactions);
+
+  final List<Transaction> recentTransactions;
+  List<Map<String, Object>> get groupedTranactionValues {
+    return List.generate(7, (index) {
+      final weekDay = DateTime.now().subtract(Duration(days: index));
+
+      double totalSum = 0.00;
+
+      for (var i = 0; i < recentTransactions.length; i++) {
+        if (recentTransactions[i].date.day == weekDay.day &&
+            recentTransactions[i].date.month == weekDay.month &&
+            recentTransactions[i].date.year == weekDay.year) {
+          totalSum += recentTransactions[i].ammout;
+        }
+      }
+      return {'day': DateFormat.E(weekDay), 'amount': totalSum};
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
       elevation: 6,
       margin: EdgeInsets.all(20),
+      child: Row(
+        children: [],
+      ),
     );
   }
 }
