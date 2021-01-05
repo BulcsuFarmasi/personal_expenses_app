7b70eef106ab04ec58219363b6f703a88b5f1035
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2021-01-05 22:52:37 +0100

    use list tile widgetin transaction list

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -35,41 +35,25 @@ class TransactionList extends StatelessWidget {
           : ListView.builder(
               itemBuilder: (context, index) {
                 return Card(
-                    child: Row(children: [
-                  Container(
-                      margin: EdgeInsets.symmetric(
-                        vertical: 15,
-                        horizontal: 10,
-                      ),
-                      padding: EdgeInsets.all(10),
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Theme.of(context).primaryColor,
-                          width: 2,
+                  elevation: 5,
+                  margin: EdgeInsets.symmetric(vertical: 8, horizontal: 5),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 30,
+                      child: Padding(
+                        padding: EdgeInsets.all(6),
+                        child: FittedBox(
+                          child: Text('\$${transactions[index].ammout.toStringAsFixed(2)}'),
                         ),
                       ),
-                      child: Text(
-                        '\$${transactions[index].ammout.toStringAsFixed(2)}',
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 20,
-                          color: Theme.of(context).primaryColor,
-                        ),
-                      )),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        transactions[index].title,
-                        style: Theme.of(context).textTheme.headline6,
-                      ),
-                      Text(
-                        DateFormat.yMMMMd().format(transactions[index].date),
-                        style: TextStyle(color: Colors.grey[400]),
-                      ),
-                    ],
-                  )
-                ]));
+                    ),
+                    title: Text(
+                      transactions[index].title,
+                      style: Theme.of(context).textTheme.headline6,
+                    ),
+                    subtitle: Text(DateFormat.yMMMMd().format(transactions[index].date)),
+                  ),
+                );
               },
               itemCount: transactions.length,
             ),
