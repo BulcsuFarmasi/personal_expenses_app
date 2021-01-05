e4c2c3a4ca95082c094b9f77b331ac0a494eb4fa
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2021-01-05 22:57:40 +0100

    improving the chart a bit

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -21,7 +21,7 @@ class Chart extends StatelessWidget {
         }
       }
       return {'day': DateFormat.E().format(weekDay).substring(0, 1), 'amount': totalSum};
-    });
+    }).reversed.toList();
   }
 
   double get totalSpending {
