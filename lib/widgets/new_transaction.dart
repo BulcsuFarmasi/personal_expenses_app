e3771257a55402619c05eb931999a742d1c21a98
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2021-01-23 16:09:13 +0100

    use cupertino button in ios instead of flat button

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -99,14 +102,22 @@ class _NewTransactionState extends State<NewTransaction> {
                             : 'Picked Date: ${DateFormat.yMd().format(_selectedDate)}',
                       ),
                     ),
-                    FlatButton(
-                      onPressed: _presentDatePicker,
-                      child: Text(
-                        'Chose date',
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                      textColor: theme.primaryColor,
-                    ),
+                    Platform.isIOS
+                        ? CupertinoButton(
+                            onPressed: _presentDatePicker,
+                            child: Text(
+                              'Chose date',
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                          )
+                        : FlatButton(
+                            onPressed: _presentDatePicker,
+                            child: Text(
+                              'Chose date',
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                            textColor: theme.primaryColor,
+                          ),
                   ],
                 ),
               ),
