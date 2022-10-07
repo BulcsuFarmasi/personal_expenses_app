c25e682d3eab47d9bfd6c8d3d1aa89c395ff53ce
Bulcsú Farmasi <bulcsu.farmasi@gmail.com>
2022-10-07 18:45:09 +0200

    make appbar widgets to implement preferred size widget

--- lib/widgets/ios_app_bar.dart
+++ lib/widgets/ios_app_bar.dart
@@ -1,23 +1,31 @@
 import 'package:flutter/cupertino.dart';
 
-class IosAppBar extends StatelessWidget {
-  const IosAppBar({super.key, required this.title, required this.addNewTransaction});
+class IosAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const IosAppBar(
+      {super.key, required this.title, required this.addNewTransaction});
+
   final String title;
   final VoidCallback addNewTransaction;
 
+  PreferredSizeWidget get appBar => CupertinoNavigationBar(
+        middle: Text(title),
+        trailing: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            GestureDetector(
+              onTap: addNewTransaction,
+              child: const Icon(CupertinoIcons.add),
+            )
+          ],
+        ),
+      );
+
   @override
   Widget build(BuildContext context) {
-    return CupertinoNavigationBar(
-      middle: Text(title),
-      trailing: Row(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          GestureDetector(
-            onTap: addNewTransaction,
-            child: const Icon(CupertinoIcons.add),
-          )
-        ],
-      ),
-    );
+    return appBar;
   }
+
+  @override
+  // TODO: implement preferredSize
+  Size get preferredSize => appBar.preferredSize;
 }
--- lib/widgets/material_app_bar.dart
+++ lib/widgets/material_app_bar.dart
@@ -1,21 +1,29 @@
 import 'package:flutter/material.dart';
 
-class MaterialAppBar extends StatelessWidget {
-  const MaterialAppBar({super.key, required this.title, required this.addNewTransaction});
+class MaterialAppBar extends StatelessWidget implements PreferredSizeWidget {
+  const MaterialAppBar(
+      {super.key, required this.title, required this.addNewTransaction});
 
   final String title;
   final VoidCallback addNewTransaction;
 
+  PreferredSizeWidget get appBar =>
+      AppBar(
+        title: Text(title),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: addNewTransaction,
+          ),
+        ],
+      );
+
   @override
   Widget build(BuildContext context) {
-    return AppBar(
-      title: Text(title),
-      actions: [
-        IconButton(
-          icon: const Icon(Icons.add),
-          onPressed: addNewTransaction,
-        ),
-      ],
-    );
+    return appBar;
   }
+
+  @override
+  // TODO: implement preferredSize
+  Size get preferredSize => appBar.preferredSize;
 }
